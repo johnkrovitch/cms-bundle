641315196be45a9cd942757ac86bd351e236f9c4
johnkrovitch <arnaudfrezet@gmail.com>
2020-06-04 22:01:09 +0200

    Remove legacy js code

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -3,7 +3,6 @@ import 'select2/dist/css/select2.min.css';
 import '../scss/cms.scss';
 import Modal from './cms/modal/Modal';
 import Select2 from "./cms/select2/Select2";
-import MediaForm from "./cms/forms/MediaForm";
 
 $(document).ready(function () {
     // Initialize select2 inputs
@@ -12,8 +11,5 @@ $(document).ready(function () {
     // Create a modal component with the default options
     let modal = new Modal();
     modal.bind();
-
-    // Initialize media form
-    let mediaForm = new MediaForm();
-    mediaForm.bind();
+    
 });
